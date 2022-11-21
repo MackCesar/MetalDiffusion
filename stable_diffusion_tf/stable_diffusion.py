dcad922a09e1a79ccc440ba7a866e08054a88809
soten355 <soten355@gmail.com>
2022-11-21 09:05:00 -0800

    Small updates

--- stable_diffusion_tf/stable_diffusion.py
+++ stable_diffusion_tf/stable_diffusion.py
@@ -10,17 +10,20 @@ import numpy as np
 import math
 import random
 
+### Memmory Management
+import gc #Garbag Collector
+
 ### Import tensorflow, but with supressed warnings
 # Filter tensorflow version warnings
 # https://stackoverflow.com/questions/40426502/is-there-a-way-to-suppress-the-messages-tensorflow-prints/40426709
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 # https://stackoverflow.com/questions/15777951/how-to-suppress-pandas-future-warning
-warnings.simplefilter(action='ignore', category=FutureWarning)
-warnings.simplefilter(action='ignore', category=Warning)
+#warnings.simplefilter(action='ignore', category=FutureWarning)
+#warnings.simplefilter(action='ignore', category=Warning)
 import tensorflow as tf
-tf.get_logger().setLevel('INFO')
-tf.autograph.set_verbosity(0)
-tf.get_logger().setLevel(logging.ERROR)
+#tf.get_logger().setLevel('INFO')
+#tf.autograph.set_verbosity(0)
+#tf.get_logger().setLevel(logging.ERROR)
 
 ### Keras module
 from tensorflow import keras
@@ -28,7 +31,6 @@ try:
    from keras import backend as K
 except Exception as e:
    print(e)
-   from tensorflow.keras import backend as K
 
 ### Modules for Machine Learning
 from .autoencoder_kl import Decoder, Encoder
@@ -47,29 +49,38 @@ MAX_TEXT_LEN = 77
 ### Main Class
 
 class StableDiffusion:
-    def __init__(self, img_height=1000, img_width=1000, jit_compile=False, download_weights=True):
-        # Necessary variables
+    def __init__(
+        self,
+        img_height = 512,
+        img_width = 512,
+        jit_compile = False,
+        download_weights =True
+    ):
+        # Set image dimensions
         self.img_height = img_height
         self.img_width = img_width
+
+        # Set tokenizer
         self.tokenizer = SimpleTokenizer()
 
         # weight and models
-        text_encoder, diffusion_model, decoder, encoder = get_models(img_height, img_width, download_weights=download_weights)
+        text_encoder, diffusion_model, decoder, encoder = get_models(img_height, img_width, download_weights = download_weights)
         self.text_encoder = text_encoder
         self.diffusion_model = diffusion_model
         self.decoder = decoder
         self.encoder = encoder
 
-        # jit compile
+        # Just in time compilation
         if jit_compile:
-            self.text_encoder.compile(jit_compile=True)
-            self.diffusion_model.compile(jit_compile=True)
-            self.decoder.compile(jit_compile=True)
+            self.text_encoder.compile(jit_compile = True)
+            self.diffusion_model.compile(jit_compile = True)
+            self.decoder.compile(jit_compile = True)
+            self.encoder.compile(jit_compile = True)
         
         # Global policy
         self.dtype = tf.float32 # Default
 
-        # Maaaybe float16 will result in faster images?
+        # Maaaybe float16 will result in faster images? Switch to float16 if global policy says so
         if tf.keras.mixed_precision.global_policy().name == 'mixed_float16':
             self.dtype = tf.float16
 
@@ -77,14 +88,15 @@ class StableDiffusion:
     def generate(
         self,
         prompt,
-        batch_size=1,
-        num_steps=25,
-        unconditional_guidance_scale=7.5,
-        temperature=1,
-        seed=None,
-        input_image=None,
-        input_image_strength=0.5,
-        negativePrompt=None
+        negativePrompt = None,
+        batch_size = 1,
+        num_steps = 25,
+        unconditional_guidance_scale = 7.5,
+        temperature = 1,
+        seed = None,
+        input_image = None,
+        input_image_strength = 0.5,
+        input_mask = None
     ):
         # Tokenize prompt (i.e. starting context)
         print("\n...tokenizing prompt...")
@@ -95,7 +107,7 @@ class StableDiffusion:
             inputs = inputs[0:76]
         phrase = inputs + [49407] * (77 - len(inputs))
         phrase = np.array(phrase)[None].astype("int32")
-        phrase = np.repeat(phrase, batch_size, axis=0)
+        phrase = np.repeat(phrase, batch_size, axis = 0)
 
         # Encode prompt tokens (and their positions) into a "context vector"
         print("...encoding the tokenized prompt...")
@@ -104,45 +116,51 @@ class StableDiffusion:
         context = self.text_encoder.predict_on_batch([phrase, pos_ids])
 
         # Prepare the input image, if it was given
-        if type(input_image) is str:
+        input_image_tensor = None
+        if input_image is not None:
             print("...preparing input image...")
-            input_image = Image.open(input_image)
-            input_image = input_image.resize((self.img_width, self.img_height))
-            input_image = np.array(input_image)[... , :3]
-            input_image = (input_image.astype("float") / 255.0)*2 - 1 
+            if type(input_image) is str:
+                input_image = Image.open(input_image)
+                input_image = input_image.resize((self.img_width, self.img_height))
+
+            elif type(input_image) is np.ndarray:
+                input_image = np.resize(input_image, (self.img_height, self.img_width, input_image.shape[2]))
+                
+            input_image_array = np.array(input_image, dtype=np.float32)[None,...,:3]
+            input_image_tensor = tf.cast((input_image_array / 255.0) * 2 - 1, self.dtype)
+        
+        # Prepare the image mask
+        if type(input_mask) is str:
+            input_mask = Image.open(input_mask)
+            input_mask = input_mask.resize((self.img_width, self.img_height))
+            input_mask_array = np.array(input_mask, dtype=np.float32)[None,...,None]
+            input_mask_array =  input_mask_array / 255.0
+            
+            latent_mask = input_mask.resize((self.img_width//8, self.img_height//8))
+            latent_mask = np.array(latent_mask, dtype=np.float32)[None,...,None]
+            latent_mask = 1 - (latent_mask.astype("float") / 255.0)
+            latent_mask_tensor = tf.cast(tf.repeat(latent_mask, batch_size , axis=0), self.dtype)
 
         # Create a random seed if one is not provided
         if seed is None:
             print("...generating random seed...")
             seed = random.randint(1000, sys.maxsize)
 
-        # Encode unconditional tokens (and their positions into an "unconditional context vector")
-        # Only do this if Negative Prompt is empty
-        if (negativePrompt is None):
-            # print("\nNo negative prompt given")
-            unconditional_tokens = np.array(_UNCONDITIONAL_TOKENS)[None].astype("int32")
-            unconditional_tokens = np.repeat(unconditional_tokens, batch_size, axis=0)
-            self.unconditional_tokens = tf.convert_to_tensor(unconditional_tokens)
-            unconditional_context = self.text_encoder.predict_on_batch(
-                [self.unconditional_tokens, pos_ids]
-            )
-        else:
-            print("...tokenizing negative prompt...")
-             # Tokenize negative prompt (i.e. starting context, but what to avoid)
-            negativeInputs = self.tokenizer.encode(negativePrompt)
-            # assert len(negativeInputs) < 77, "Negative Prompt is too long (should be < 77 tokens)"
-            if len(negativeInputs) > 77:
-                print("Negative prompt is too long (should be < 77 tokens). Truncating down to 77 tokens")
-                negativeInputs = negativeInputs[0:76]
-            negPhrase = negativeInputs + [49407] * (77 - len(negativeInputs))
-
-            print("...encoding negative prompt...")
-            negPhrase = np.array(negPhrase)[None].astype("int32")
-            negPhrase = np.repeat(negPhrase, batch_size, axis=0)
-            unconditional_context = self.text_encoder.predict_on_batch(
-                [negPhrase, pos_ids]
-            )
-
+        # Tokenize negative prompt or use default padding tokens
+        unconditional_tokens = _UNCONDITIONAL_TOKENS
+        if negativePrompt is not None:
+            inputs = self.tokenizer.encode(negativePrompt)
+            if len(inputs) > 77:
+                print("Prompt is too long (should be < 77 tokens). Truncating down to 77 tokens")
+                inputs = inputs[0:76]
+            unconditional_tokens = inputs + [49407] * (77 - len(inputs))
+        
+        # Encode unconditional tokens (and their positions into an "unconditional context vector"
+        unconditional_tokens = np.array(unconditional_tokens)[None].astype("int32")
+        unconditional_tokens = np.repeat(unconditional_tokens, batch_size, axis=0)
+        unconditional_context = self.text_encoder.predict_on_batch(
+            [unconditional_tokens, pos_ids]
+        )
 
         # Establish time steps
         print("...establishing time steps...")
@@ -151,8 +169,14 @@ class StableDiffusion:
         # Input image time steps
         print("...establishing input image time steps...")
         input_img_noise_t = timesteps[ int(len(timesteps)*input_image_strength) ]
+
+        # Get starting parameters for generation
         latent, alphas, alphas_prev = self.get_starting_parameters(
-            timesteps, batch_size, seed , input_image=input_image, input_img_noise_t=input_img_noise_t
+            timesteps,
+            batch_size,
+            seed,
+            input_image = input_image_tensor,
+            input_img_noise_t = input_img_noise_t
         )
 
         if input_image is not None:
@@ -176,21 +200,41 @@ class StableDiffusion:
                 latent, e_t, index, a_t, a_prev, temperature, seed
             )
 
+            if input_mask is not None and input_image is not None:
+                # If mask is provided, noise at current timestep will be added to input image.
+                # The intermediate latent will be merged with input latent.
+                latent_orgin, alphas, alphas_prev = self.get_starting_parameters(
+                    timesteps,
+                    batch_size,
+                    seed,
+                    input_image = input_image_tensor,
+                    input_img_noise_t = timestep
+                )
+                latent = latent_orgin * latent_mask_tensor + latent * (1- latent_mask_tensor)
+
         # Decoding stage
         print("\n...decoding diffusion...")
         decoded = self.decoder.predict_on_batch(latent)
         decoded = ((decoded + 1) / 2) * 255
+
+        # Merging of inpainting result of input mask with original image
+
+        if input_mask is not None:
+          # Merge inpainting output with original image
+          decoded = input_image_array * (1-input_mask_array) + np.array(decoded) * input_mask_array
+        
+        #Memory cleanup
+        gc.collect()
+
+        # return final image as an array
         return np.clip(decoded, 0, 255).astype("uint8")
 
-    def add_noise(self, x , t ):
-        batch_size,w,h = x.shape[0] , x.shape[1] , x.shape[2]
-        noise = tf.random.normal((batch_size,w,h,4))
-        sqrt_alpha_prod = _ALPHAS_CUMPROD[t] ** 0.5
-        sqrt_one_minus_alpha_prod = (1 - _ALPHAS_CUMPROD[t]) ** 0.5
-
-        return  sqrt_alpha_prod * x + sqrt_one_minus_alpha_prod * noise
-
-    def timestep_embedding(self, timesteps, dim=320, max_period=10000):
+    def timestep_embedding(
+        self,
+        timesteps,
+        dim = 320,
+        max_period = 10000
+    ):
         half = dim // 2
         freqs = np.exp(
             -math.log(max_period) * np.arange(0, half, dtype="float32") / half
@@ -198,6 +242,37 @@ class StableDiffusion:
         args = np.array(timesteps) * freqs
         embedding = np.concatenate([np.cos(args), np.sin(args)])
         return tf.convert_to_tensor(embedding.reshape(1, -1))
+
+
+    def add_noise(self, x , t , noise = None ):
+        batch_size,w,h = x.shape[0] , x.shape[1] , x.shape[2]
+        if noise is None:
+            noise = tf.random.normal((batch_size,w,h,4), dtype=self.dtype)
+        sqrt_alpha_prod = _ALPHAS_CUMPROD[t] ** 0.5
+        sqrt_one_minus_alpha_prod = (1 - _ALPHAS_CUMPROD[t]) ** 0.5
+
+        return  sqrt_alpha_prod * x + sqrt_one_minus_alpha_prod * noise
+
+    def get_starting_parameters(
+        self,
+        timesteps,
+        batch_size,
+        seed, 
+        input_image = None,
+        input_img_noise_t = None
+    ):
+        n_h = self.img_height // 8
+        n_w = self.img_width // 8
+        alphas = [_ALPHAS_CUMPROD[t] for t in timesteps]
+        alphas_prev = [1.0] + alphas[:-1]
+        if input_image is None:
+            latent = tf.random.normal((batch_size, n_h, n_w, 4), seed=seed)
+        else:
+            latent = self.encoder(input_image)
+            latent = tf.repeat(latent , batch_size , axis = 0)
+            latent = self.add_noise(latent, input_img_noise_t)
+        return latent, alphas, alphas_prev
+
 
     def get_model_output(
         self,
@@ -210,7 +285,7 @@ class StableDiffusion:
     ):
         timesteps = np.array([t])
         t_emb = self.timestep_embedding(timesteps)
-        t_emb = np.repeat(t_emb, batch_size, axis=0)
+        t_emb = np.repeat(t_emb, batch_size, axis = 0)
         unconditional_latent = self.diffusion_model.predict_on_batch(
             [latent, t_emb, unconditional_context]
         )
@@ -229,24 +304,11 @@ class StableDiffusion:
         noise = sigma_t * tf.random.normal(x.shape, seed=seed) * temperature
         x_prev = math.sqrt(a_prev) * pred_x0 + dir_xt
         return x_prev, pred_x0
-
-    def get_starting_parameters(self, timesteps, batch_size, seed,  input_image=None, input_img_noise_t=None):
-        n_h = self.img_height // 8
-        n_w = self.img_width // 8
-        alphas = [_ALPHAS_CUMPROD[t] for t in timesteps]
-        alphas_prev = [1.0] + alphas[:-1]
-        if input_image is None:
-            latent = tf.random.normal((batch_size, n_h, n_w, 4), seed=seed)
-        else:
-            latent = self.encoder(input_image[None])
-            latent = self.add_noise(latent, input_img_noise_t)
-            latent = tf.repeat(latent , batch_size , axis=0)
-        return latent, alphas, alphas_prev
     
     # Load pytorch weights as models
-    def load_weights_from_pytorch_ckpt(self , pytorch_ckpt_path):
+    def load_weights_from_pytorch_ckpt(self, pytorch_ckpt_path):
         print("\nLoading pytorch checkpoint " + pytorch_ckpt_path)
-        pt_weights = torch.load(pytorch_ckpt_path, map_location="cpu")
+        pt_weights = torch.load(pytorch_ckpt_path, map_location = "cpu")
         for module_name in ['text_encoder', 'diffusion_model', 'decoder', 'encoder' ]:
             module_weights = []
             for i , (key , perm ) in enumerate(PYTORCH_CKPT_MAPPING[module_name]):
@@ -261,7 +323,7 @@ class StableDiffusion:
 
 # Get models if we don't already have them
 
-def get_models(img_height, img_width, download_weights=True):
+def get_models(img_height, img_width, download_weights = True):
     n_h = img_height // 8
     n_w = img_width // 8
 
@@ -325,7 +387,3 @@ def get_models(img_height, img_width, download_weights=True):
 
         print("...weights loaded!")
     return text_encoder, diffusion_model, decoder , encoder
-
-def publicPrint(text):
-    print(text)
-    return text
